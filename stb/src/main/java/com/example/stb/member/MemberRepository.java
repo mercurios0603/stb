2882527e070277211835c4cb7488407814ffd3fc
mercurios0603 <mercurios0603@gmail.com>
2023-10-30 17:50:51 +0900

    stb day39 end

--- stb/src/main/java/com/example/stb/member/MemberRepository.java
+++ stb/src/main/java/com/example/stb/member/MemberRepository.java
@@ -3,5 +3,8 @@ package com.example.stb.member;
 import com.example.stb.article.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+  Optional<Member> findByusername(String username);
 }
---
+++ stb/src/main/java/com/example/stb/member/MemberSecurityService.java
@@ -0,0 +1,39 @@
+package com.example.stb.member;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class MemberSecurityService implements UserDetailsService {
+
+  private final MemberRepository memberRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<Member> _siteUser = this.memberRepository.findByusername(username);
+    if (_siteUser.isEmpty()) {
+      throw new UsernameNotFoundException("사용자를 찾을수 없습니다.");
+    }
+    Member member = _siteUser.get();
+    List<GrantedAuthority> authorities = new ArrayList<>();
+    //MemberRole.ADMIN과 MemberRole.USER는 enum 자료형 때문에 가능한 것.
+    if ("admin".equals(username)) {
+      authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
+    } else {
+      authorities.add(new SimpleGrantedAuthority(MemberRole.USER.getValue()));
+    }
+    return new User(member.getUsername(), member.getPassword(), authorities);
+  }
+}
